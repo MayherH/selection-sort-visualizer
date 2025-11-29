5561be0e8f2ceba75c3e96cd2152469f08e1f78e
Maher <24wfv1@queensu.ca>
2025-11-28 19:26:43 -0500

    Added steps and snapshots
    added a list that appends a snapshot of each state that the search is in (current array, i value, j value, minimum value). I also just realized that my last commit was made on the wrong account, its still my account though

--- sort.py
+++ sort.py
@@ -1,9 +1,15 @@
 def selection_sort(arr):
+    
+    steps = []
     n = len(arr)
     for i in range(n):
         min_idx = i
         for j in range(i + 1, n):
+            steps.append({"array": arr.copy(), "i": i, "j": j, "min_idx": min_idx})
             if arr[j] < arr[min_idx]:
                 min_idx = j
+                steps.append({"array": arr.copy(), "i": i, "j": j, "min_idx": min_idx})
+        steps.append({"array": arr.copy(), "i": i, "j": None, "min_idx": min_idx})
         arr[i], arr[min_idx] = arr[min_idx], arr[i]
-    return arr
+        steps.append({"array": arr.copy(), "i": i, "j": None, "min_idx": min_idx})
+    return steps
